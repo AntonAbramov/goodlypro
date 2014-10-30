8e4b2dc97d6e9d84d98a282d85899f302e4c603a
anton <AntonAbramov1990@gmail.com>
2014-10-30 22:45:34 +0200

    fixes

--- js/init.js
+++ js/init.js
@@ -6,6 +6,21 @@ $(document).ready(function () {
   if($('select').length){
     $('select, .select').selectric();
   }
+
+  $('.choose-list').find('li').find('label').on("click", function(event){
+    if(!$(this).parent().hasClass("is-open")) {
+      $('.choose-list').find('li').removeClass('is-open');
+      $(this).parent().addClass('is-open');
+    }
+  });
+  $('.enter-code-box').find('input').on('change', function(){
+    if ($(this).is(':checked')) {
+      $('.enter-code-box').addClass('is-active');
+    }
+    else {
+      $('.enter-code-box').removeClass('is-active');
+    }
+  })
 });
 
 $(window).resize(function () {
